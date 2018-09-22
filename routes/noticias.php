c322f1c4158854337a9eab281390bfc46ee7cef4
PauloMtz <pramosmtz@gmail.com>
2018-09-22 19:18:51 -0300

    Complementação de comentários

--- routes/noticias.php
+++ routes/noticias.php
@@ -3,10 +3,15 @@ $this->get('noticias', function($arg) {
 	echo 'Últimas Notícias';
 });
 $this->get('noticias/{id}', function($arg) {
+
+	// carrega o módulo Template
 	$templ = $this->core->loadModule('template');
 
+	// envia dados para o view
 	$num = array('numero' => $arg['id']);
 
+	// carrega o template teste.php, localizado na pasta templates
+	// a pasta templates é utilizada para os arquivos de visualização do usuário
 	$templ->render('teste', $num);
 });
 ?>
